fa2d9fe91504226647e76fa5485a6eb2a890c507
Ryan Gibson <rgibson@redhat.com>
2023-06-01 02:17:44 -0400

    Just keep it all in a single array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // App.tsx
-
 import React from "react";
 import CardComponent from "./CardComponent";
 import PlayerComponent from "./PlayerComponent";
@@ -31,56 +30,41 @@ function App() {
                 cards.push({ suit: suit as any, rank: rank });
             }
         }
-
         // shuffle the cards using the helper function
         let shuffledCards = shuffleCards(cards);
-
         // create an array of players with names and hands
-        let players: Player[] = [
+        let allPlayers: Player[] = [
             { name: "Player1", hand: [], isHuman: true },
             { name: "Player2", hand: [], isHuman: false },
             { name: "Player3", hand: [], isHuman: false },
             { name: "Player4", hand: [], isHuman: false },
         ];
-
         // deal five cards to each player using the helper function and update their hand
-        let dealtPlayers = dealCards(shuffledCards, players);
-
+        let dealtPlayers = dealCards(shuffledCards, allPlayers);
         // determine the first dealer using the helper function and assign it to dealer index state hook
-        let firstDealerIndex = determineFirstDealer(shuffledCards, players);
-
+        let firstDealerIndex = determineFirstDealer(shuffledCards, allPlayers);
         // set the game started state hook to true
         setGameStarted(true);
-
         // set the table state hook to have the top card as the last card from the shuffled cards array
         setTable({ topCard: shuffledCards[shuffledCards.length - 1] });
-
         // set the players state hook to the dealt players array
         setPlayers(dealtPlayers);
-
         // set the teams state hook to have two teams with names and scores
         setTeams([
             { name: "Team1", score: 0 },
             { name: "Team2", score: 0 },
         ]);
-
         // set the dealer index state hook to the first dealer index
         setDealerIndex(firstDealerIndex);
         // log the dealer index value
-        console.log("dealer index:", dealerIndex);
-
-        // increment the dealer index by one (or reset to zero if it reaches four) and assign it to turn index state hook
-        setTurnIndex((firstDealerIndex! + 1) % 4);
+        console.log("dealer index:", firstDealerIndex);
+        // increment the first dealer index by one (or reset to zero if it reaches four) and assign it to turn index state hook
+        setTurnIndex((firstDealerIndex + 1) % 4);
         // log the turn index value
-        console.log("turn index:", turnIndex);
-
+        console.log("turn index:", (firstDealerIndex + 1) % 4);
         // set trumpSuit state hook based on topCard suit or player choice
         setTrumpSuit(shuffledCards[shuffledCards.length - 1].suit);
     };
-
-    {/* filter players by isHuman property */}
-    const humanPlayer = players.filter((player) => player.isHuman)[0];
-    const computerPlayers = players.filter((player) => !player.isHuman);
 
     return (
         <div className="app">
@@ -94,17 +78,9 @@ function App() {
                     <div className="table">
                         {table.topCard && <CardComponent card={table.topCard} />}
                     </div>
-
-                    {/* render human-player and computer-players divs */}
-                    <div className="human-player">
-                        <PlayerComponent
-                            player={humanPlayer}
-                            isDealer={dealerIndex === 0}
-                            isTurn={turnIndex === 0}
-                        />
-                    </div>
-                    <div className="computer-players">
-                        {computerPlayers.map((player, index) => (
+                    {/* render all-players div */}
+                    <div className="all-players">
+                        {players.map((player, index) => (
                             <PlayerComponent
                                 key={player.name}
                                 player={player}
@@ -113,37 +89,27 @@ function App() {
                             />
                         ))}
                     </div>
-                    
                     {/* pass team prop to TeamComponent */}
                     <div className="teams">
                         <TeamComponent team={teams[0]} />
                         <TeamComponent team={teams[1]} />
                     </div>
-
                     {/* In the top right of the screen, the score of each team is displayed in a box. */}
-
                     <div className="score">
                         <p>Team1: {teams[0].score}</p>
                         <p>Team2: {teams[1].score}</p>
                     </div>
-
                     {/* In the top left of the screen, the trump is displayed in a box. */}
-
                     <div className="trump">{trumpSuit && <p>Trump: {trumpSuit}</p>}</div>
-
                     {/* In the bottom left of the screen, the name of the player who is the dealer is displayed in a box. */}
-
                     <div className="dealer">
                         {dealerIndex !== null && (
                             <p>Dealer: {players[dealerIndex].name}</p>
                         )}
                     </div>
-
                     {/* In the bottom right of the screen, the name of the player whose turn it is to play a card is displayed in a box. */}
                     <div className="turn">
-                        {turnIndex !== null && (
-                            <p>Turn: {players[turnIndex].name}</p>
-                        )}
+                        {turnIndex !== null && <p>Turn: {players[turnIndex].name}</p>}
                     </div>
                 </div>
             )}
