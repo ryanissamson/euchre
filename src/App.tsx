2467ba6eb17bff6cc4cae6bb4838ed91fc2276c2
Ryan Gibson <rgibson@redhat.com>
2023-06-01 01:16:21 -0400

    How to cheat and get a perfect hand every time.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PlayerComponent from "./PlayerComponent";
 import TeamComponent from "./TeamComponent";
 import { Card, Player, Team } from "./types";
 import { shuffleCards, dealCards, determineFirstDealer } from "./utils";
+import "./App.css";
 
 function App() {
     // initialize state hooks for game started, table, players, teams, dealer index, turn index and trump suit
@@ -45,14 +46,43 @@ function App() {
         // deal five cards to each player using the helper function and update their hand
         let dealtPlayers = dealCards(shuffledCards, players);
 
+        // Check each player's hand and log to console if a player has cards of the same suit and matches the top card.
+        // If no player has cards of the same suit, deal the cards again until someone does. When a player has cards of the same suit, log to console that dealing is complete. Also log to console the cards in the hand of the player that matches.
+        let sameSuit = false;
+        while (!sameSuit) {
+            for (let player of dealtPlayers) {
+                let suits = player.hand.map((card) => card.suit);
+                if (suits.every((suit) => suit === suits[0])) {
+                    sameSuit = true;
+                    console.log("Dealing complete!");
+                    console.log(dealtPlayers);
+                    console.log(shuffledCards)
+                    // Check if it matches the top card, and if not, shuffle the cards and deal again.
+                    if (suits[0] !== shuffledCards[shuffledCards.length - 1].suit) {
+                        sameSuit = false;
+                        shuffledCards = shuffleCards(cards);
+                        dealtPlayers = dealCards(shuffledCards, players);
+                        console.log("Dealing again...");
+                        break;
+                    }
+                    break;
+                }
+            }
+            if (!sameSuit) {
+                shuffledCards = shuffleCards(cards);
+                dealtPlayers = dealCards(shuffledCards, players);
+                console.log("Dealing again...");
+            }
+        }
+
         // determine the first dealer using the helper function and assign it to dealer index state hook
         let firstDealerIndex = determineFirstDealer(shuffledCards, players);
 
         // set the game started state hook to true
         setGameStarted(true);
 
-        // set the table state hook to have the top card as the sixth card from the shuffled cards array
-        setTable({ topCard: shuffledCards[5] });
+        // set the table state hook to have the top card as the last card from the shuffled cards array
+        setTable({ topCard: shuffledCards[shuffledCards.length - 1] });
 
         // set the players state hook to the dealt players array
         setPlayers(dealtPlayers);
